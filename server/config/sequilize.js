616b339d3243c65b5c0008063c0b786b4f48dcd5
Jairo Bernal <jairo.bernal@send.sd>
2020-02-12 15:00:29 -0500

    use user env variable

--- server/config/sequilize.js
+++ server/config/sequilize.js
@@ -6,6 +6,7 @@ export default {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    use_env_variable: process.env.DATABASE_URL
   }
 }
